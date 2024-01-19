b571db4f51a83138cb3cddf28896fb34d0632e79
exerouze <jafarabdullah586@gmail.com>
2024-01-19 22:08:33 +0700

    Refactoring function

--- index.js
+++ index.js
@@ -6,14 +6,22 @@ app.set('view engine', 'hbs')
 app.set('views', 'src/views')
 
 app.use('/assets', express.static('src/assets'))
+app.use(express.urlencoded({extended: false})) //body parser
 
 app.get('/', home)
-
 app.get('/index', home)
 app.get('/contact', contact)
 app.get('/project', project)
 app.get('/oop', oop)
 app.get('/detail-project/:id', detailProject)
+app.post('/project', handleProject)
+app.get("/delete/:id", deleteProject);
+
+app.get("/edit-project/:id", editProject);
+app.post("/edit-project/:id", editProjectData);
+
+
+const data = []
 
 function home(req, res){
   res.render('index')
@@ -24,40 +32,106 @@ function contact(req, res){
 }
 
 function project(req, res){
-  const data = [
-     {
-      id:1,
-      title : 'Data 1',
-      content : 'Content 1'
-     },
-     {
-      id:2,
-      title : 'Data 2',
-      content : 'Content 2'
-     },
-     {
-      id:3,
-      title : 'Data 3',
-      content : 'Content 3'
-     }
+  const titleWeb = 'My Project'
 
-  ]
     
-
-  res.render('project', {data})
+  res.render('project', {data, titleWeb})
 }
 
 function detailProject(req, res){
   const {id} = req.params
 
   console.log(id);
+  const dataDetailProject = data[id];
 
-  res.render('detail-project', {id})
+  res.render("detail-project", { data: dataDetailProject});
+
 }
+
+function handleProject(req, res){
+  const {nameProject, startDate, endDate, descProject, tech} = req.body
+
+  const firstDate = new Date(startDate);
+  const secondDate = new Date(endDate);
+  const time = Math.abs(firstDate - secondDate);
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+  let interval = '';
+
+  if (days < 30) {
+    interval = days + " Days";
+  } else {
+    const months = Math.floor(days / 30);
+    const remainingDays = days % 30;
+    interval = months + " Months";
+    
+    if (remainingDays > 0) {
+      interval += ` ${remainingDays} Days`;
+    }
+  }
+  
+  data.unshift({
+    nameProject,startDate,endDate,descProject,interval,
+    tech: Array.isArray(tech) ? tech :[tech]
+  })
+
+  res.redirect('/project')
+
+}
+
+function deleteProject(req, res) {
+  const { id } = req.params;
+  data.splice(id, 1);
+
+  res.redirect("/project");
+}
+
+function editProject(req, res) {
+  const { id } = req.params;
+  const dataEditProject = data[+id];
+  dataEditProject.id = id;
+
+  res.render("edit-project", { data: dataEditProject });
+}
+
+function editProjectData(req, res){
+  const { id } = req.params;
+  const {nameProject, startDate, endDate, descProject, tech} = req.body
+
+  const firstDate = new Date(startDate);
+  const secondDate = new Date(endDate);
+  const time = Math.abs(firstDate - secondDate);
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+  let interval = '';
+
+  if (days < 30) {
+    interval = days + " Days";
+  } else {
+    const months = Math.floor(days / 30);
+    const remainingDays = days % 30;
+    interval = months + " Months";
+    
+    if (remainingDays > 0) {
+      interval += ` ${remainingDays} Days`;
+    }
+  }
+  
+    data[+id]={
+     nameProject,startDate,endDate,descProject,interval,
+      tech: Array.isArray(tech) ? tech :[tech]
+  }
+
+  res.redirect('/project')
+
+}
+
 
 function oop(req, res){
   res.render('oop')
 }
+
+
 
 
 
